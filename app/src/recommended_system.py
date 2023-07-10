7894b201780200649aa69180f2e06fa5d3439304
jaynamm <polky0421@gmail.com>
2023-07-10 15:45:03 +0900

    Fix: type error 수정

--- app/src/recommended_system.py
+++ app/src/recommended_system.py
@@ -66,7 +66,7 @@ def post_recommended_data(input_data):
         user_profile[skill] = grade
     
     # 유사도 검증
-    skill_df['similarity_score'] = skill_df.apply(lambda row: sum(0 if (row[column].astype(str) > user_profile[column].astype(str)).all() else (row[column] / user_profile[column]) for column in skill_df.columns[1:]) / len(skill_df.columns), axis=1)
+    skill_df['similarity_score'] = skill_df.apply(lambda row: sum(0 if (row[column].astype(str) > user_profile[column].astype(str)).all() else (int(row[column]) / int(user_profile[column])) for column in skill_df.columns[1:]) / len(skill_df.columns), axis=1)
 
     recommended_jobs = skill_df.nlargest(20, 'similarity_score')
     
