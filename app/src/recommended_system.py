e7cfa1743ada0917c66b02c149bcdb0a1bd363fa
jaynamm <polky0421@gmail.com>
2023-07-10 19:57:42 +0900

    refactoring

--- app/src/recommended_system.py
+++ app/src/recommended_system.py
@@ -5,58 +5,50 @@ import json
 from src.property import db_connect
 
 
-def get_result_for_ccd_loc(location):
-    conn = db_connect.connect()
-    
+def get_result_for_ccd_loc(conn, location):
     ccd_query = "SELECT id, url, job_list, title, company, location FROM combined_crawling_data WHERE location LIKE %s"
     ccd_df = pd.read_sql(ccd_query, conn, params=(f"%{location}%"))
-    
-    conn.close()
 
     return ccd_df
 
-def get_result_for_ccd_job_loc(job_name, location):
-    conn = db_connect.connect()
-    
+def get_result_for_ccd_job_loc(conn, job_name, location):
     ccd_query = "SELECT id, url, job_list, title, company, location FROM combined_crawling_data WHERE job_list LIKE %s AND location LIKE %s"
     ccd_df = pd.read_sql(ccd_query, conn, params=(f"%{job_name}%", f"%{location}%"))
-    
-    conn.close()
 
     return ccd_df
 
-def get_scaled_feature():
-    conn = db_connect.connect()
-    
+def get_scaled_feature(conn):
     qps_query = "SELECT * FROM scaled_quals_prefs_skills"
-    
     feature = pd.read_sql(qps_query, conn)
-    
-    conn.close()
 
     return feature
 
 
 def post_recommended_data(input_data):
-    
-    feature = get_scaled_feature()
-    
     job_name = input_data['job_name']
     location = input_data['location']
     skills = input_data['skills']
+    
+    conn = db_connect.connect()
+    
+    feature = get_scaled_feature(conn)
 
     filtered_df = pd.DataFrame()
 
     if job_name == "all":
-        filtered_df = get_result_for_ccd_loc(location)
+        filtered_df = get_result_for_ccd_loc(conn, location)
     else:
-        filtered_df = get_result_for_ccd_job_loc(job_name, location)
+        filtered_df = get_result_for_ccd_job_loc(conn, job_name, location)
+        
+    conn.close()
 
     # 전체 채용 데이터에서 filtering 된 id 값에 포함되는 데이터만 정리
     feature_filtered = feature[feature['id'].isin(filtered_df['id'].tolist())]
 
     # filtering 된 데이터에서 user가 가진 skill 데이터 컬럼만 선택 (id 포함)
     skill_df = feature_filtered[['id']+list(skills.keys())]
+    
+    print(skill_df)
     
     # id 제외한 user의 skill 셋
     user_profile = pd.DataFrame(0, index=[0], columns=list(skills.keys())[1:]) 
@@ -67,9 +59,12 @@ def post_recommended_data(input_data):
         user_profile[skill] = grade
     
     # 유사도 검증
-    skill_df['similarity_score'] = skill_df.apply(lambda row: sum(0 if (row[column].astype(str) > user_profile[column].astype(str)).all() else (int(row[column]) / int(user_profile[column])) for column in skill_df.columns[1:]), axis=1)
+    skill_df['similarity_score'] = skill_df.apply(
+        lambda row: sum(0 if (row[column].astype(str) > user_profile[column].astype(str)).all() else (int(row[column]) / int(user_profile[column])) for column in skill_df.columns[1:]), 
+        axis=1
+    )
 
-    recommended_jobs = skill_df.nlargest(10, 'similarity_score')
+    recommended_jobs = skill_df.nlargest(20, 'similarity_score')
     recommended_job_lst = recommended_jobs['id'].tolist()
 
     # 딕셔너리 형태로 결과 담기
